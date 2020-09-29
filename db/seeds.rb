c6409dc84bacebd64cd1aa5322fde8fd6a8e70f7
Nykarri <karri.bonez@gmail.com>
2020-09-29 08:57:29 -0400

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.create ([
+    {post_id: 5, user_id: 1, content: Faker::Lorem.sentences(number: 1), author: 'cutiewho' },
+    {post_id: 5, user_id: 3, content: Faker::Lorem.sentences(number: 2), author: 'greenmachine' },
+    {post_id: 5, user_id: 1, content: Faker::Lorem.sentences(number: 1), author: 'cutiewho' },
+    {post_id: 1, user_id: 2, content: Faker::Lorem.sentences(number: 2), author: 'peepingTom' },
+    {post_id: 1, user_id: 3, content: Faker::Lorem.sentences(number: 1), author: 'greenmachine' },
+    {post_id: 3, user_id: 2, content: Faker::Lorem.sentences(number: 3), author: 'peepingTom' }
+])
+
+puts 'Seeded Database'
