08d435012e6a904dba35205b1cf9f531e85452f2
Nykarri <karri.bonez@gmail.com>
2020-10-05 07:13:02 -0400

    added new route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :comments do
     collection do
-      get '/:id/post', to: 'comments#post_comments'
+      get '/:post_id/post', to: 'comments#post_comments'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
